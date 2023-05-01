b75cfa0bc46543d6418987ace865e4022cbca603
Solo <solomonabu90@gmail.com>
2023-05-01 13:42:51 +0100

    init

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+function Navbar(props) {
+    return (
+        <div>Navbar</div>
+    )
+}
+
+Navbar.propTypes = {}
+
+export default Navbar
