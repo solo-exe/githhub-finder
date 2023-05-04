74f627c899c5e257590db7c6ea27f66dbdfe1b0a
Solo <solomonabu90@gmail.com>
2023-05-04 22:12:50 +0100

    updates

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { FaGithub } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-function Navbar(props) {
+
+function Navbar({ title }) {
     return (
-        <div>Navbar</div>
+        <div className='navbar mb-12 shadow-lg | bg-neutral text-neutral-content'>
+            <div className="container justify-between  mx-auto">
+
+                <div className="flex-none px-2 mx-2">
+                    <FaGithub className='inline pr-2 text-5xl' />
+                    <Link to='/' className='text-lg font-bold align-middle' >
+                        {title}
+                    </Link>
+                </div>
+
+                <div className="flex px-2 mx-2">
+                    <div className="flex justify-end">
+                        <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>Home</Link>
+                    </div>
+                    <div className="flex justify-end">
+                        <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>About</Link>
+                    </div>
+                </div>
+
+            </div>
+        </div >
     )
 }
 
-Navbar.propTypes = {}
+Navbar.defaultProps = {
+    title: 'Github Finder'
+}
+
+Navbar.protoTypes = {
+    title: PropTypes.string,
+}
 
 export default Navbar
